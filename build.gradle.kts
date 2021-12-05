070ea8c27890f4f65db09d51f9c86cdbce77f149
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2021-12-05 17:11:19 -0500

    day 1 leggooo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.6.0"
 }
@@ -9,11 +11,22 @@ repositories {
 tasks {
     sourceSets {
         main {
-            java.srcDirs("src")
+            java.srcDirs("src/main/kotlin")
         }
     }
 
     wrapper {
         gradleVersion = "7.3"
     }
+}
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
---
+++ src/main/kotlin/days/Day01.kt
@@ -0,0 +1,35 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val it = input.map { it.toInt() }.iterator()
+        var count = 0
+        var previousDepthMeasurement = it.next()
+        while (it.hasNext()) {
+            val depthMeasurement = it.next()
+            if (depthMeasurement > previousDepthMeasurement) count++
+            previousDepthMeasurement = depthMeasurement
+        }
+        return count
+    }
+
+    fun part2(input: List<String>): Int {
+        var count = 0
+        val windows = input.map { it.toInt() }.windowed(3, 1)
+        val it = windows.map { it.sum() }.iterator()
+        var previousSummedMeasurement = it.next()
+        while (it.hasNext()) {
+            val summedMeasurement = it.next()
+            if (summedMeasurement > previousSummedMeasurement) count++
+            previousSummedMeasurement = summedMeasurement
+        }
+        return count
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
+
+    val input = readInput("Day01")
+    println(part1(input))
+    println(part2(input))
+}
